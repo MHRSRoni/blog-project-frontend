5627035547d6fcfb59753fcacdd6bbdd3be8302a
Iftekhar2000 <iftekharulhaque2000@gmail.com>
2023-12-19 21:42:18 +0600

    all conflicts resolved

--- src/components/Like/Like.jsx
+++ src/components/Like/Like.jsx
@@ -46,12 +46,73 @@
 
 
 
+// import { ThumbsUp, CloudArrowUp } from "phosphor-react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { updateLike } from "../../redux/post/postSlice";
+// import axios from "../../utilities/axiosInstance";
+// import { singlePostUpdateLike } from "../../redux/singlePost/singlePostSlice";
+import banglaNumber from "../../utilities/banglaNumber";
+// import { useState } from "react";
+// import { useNavigate } from 'react-router-dom';
+
+// const Like = ({ react, postId, sliceType }) => {
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const { user } = useSelector((state) => state.auth);
+//   const userId = user?.data?._id;
+//   const isLiked = react.reactUserId.find((id) => id === userId);
+
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleLike = async () => {
+//     try {
+//       if (!userId) {
+//         return setShowModal(true);
+//       }
+
+//       await axios.post(`/post/react/${postId}/like`);
+
+//       if (sliceType === "postSlice") {
+//         dispatch(updateLike({ postId, userId }));
+//       }
+
+//       if (sliceType === "readSingleBlogSlice") {
+//         dispatch(singlePostUpdateLike({ postId, userId }));
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const onClickModalCancel = () => {
+//     setShowModal(false);
+//   };
+
+//   const onClickModalLogin = () => {
+//     setShowModal(false);
+//     navigate("/login");
+//   };
+
+//   return (
+//     <button
+//       className="flex flex-row items-center text-md ml-2 rounded-md pr-4 pl-4 pt-2 pb-2 hover:bg-[#f5f5f5] "
+//       onClick={handleLike}
+//     >
+//       <ThumbsUp size={24} style={{ color: `${isLiked ? "red" : ""}` }} />
+//       <span className="pl-2">{banglaNumber(react.like)} লাইক</span>
+//     </button>
+//   );
+// };
+
+// export default Like;
+
+
+
 import { ThumbsUp, CloudArrowUp } from "phosphor-react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateLike } from "../../redux/post/postSlice";
 import axios from "../../utilities/axiosInstance";
 import { singlePostUpdateLike } from "../../redux/singlePost/singlePostSlice";
-import banglaNumber from "../../utilities/banglaNumber";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
@@ -94,14 +155,41 @@ const Like = ({ react, postId, sliceType }) => {
   };
 
   return (
-    <button
-      className="flex flex-row items-center text-md ml-2 rounded-md pr-4 pl-4 pt-2 pb-2 hover:bg-[#f5f5f5] "
-      onClick={handleLike}
-    >
-      <ThumbsUp size={24} style={{ color: `${isLiked ? "red" : ""}` }} />
-      <span className="pl-2">{banglaNumber(react.like)} লাইক</span>
-    </button>
+    <>
+      <button
+        className="flex flex-row items-center text-md ml-2 rounded-md pr-4 pl-4 pt-2 pb-2 hover:bg-[#f5f5f5] "
+        onClick={handleLike}
+      >
+        <ThumbsUp size={24} style={{ color: `${isLiked ? "red" : ""}` }} />
+        <span className="pl-2">{banglaNumber(react.like)} লাইক</span>
+      </button>
+
+      {showModal && (
+        <div className="fixed inset-0 overflow-y-auto z-50 flex items-center justify-center">
+          <div className="flex fixed inset-0 bg-black bg-opacity-50"></div>
+          <div className="bg-white w-96 p-4 rounded-md z-10">
+            <CloudArrowUp size={28} color="#1B4DFF" />
+            <p className="text-body-5 md:text-body-4 leading-relaxed text-metal-500">
+              You are not logged in. Please login first.
+            </p>
+            <button
+              className="mt-4 px-4 py-2 bg-[#1B4DFF] text-white rounded-md"
+              onClick={onClickModalCancel}
+            >
+              Close
+            </button>
+            <button
+              className="mt-4 px-4 py-2 bg-[#1B4DFF] text-white rounded-md ml-2"
+              onClick={onClickModalLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 export default Like;
+
--- src/pages/user/ReadList.jsx
+++ src/pages/user/ReadList.jsx
@@ -1,91 +1,167 @@
-import { Button } from "keep-react";
+// import { Button } from "keep-react";
+// import { useDispatch, useSelector } from "react-redux";
+// import Spinner from "../../components/Spinner/Spinner";
+
+// import ConfirmationModal from "../../components/Modal/ConfirmationModal";
+// import { useState } from "react";
+// import { updateReadListThunk } from "../../redux/readList/readListSlice";
+
+// const ReadList = () => {
+//   const dispatch = useDispatch();
+//   const [show, setShow] = useState(false);
+//   const [postId, setPostId] = useState("");
+//   const { readList, isLoading } = useSelector((state) => state.readList);
+
+//   const handleUpdateThunk = () => {
+//     dispatch(updateReadListThunk(postId));
+//   };
+
+//   const handleShow = (postId) => {
+//     setPostId(postId);
+//     setShow(!show);
+//   };
+
+//   return (
+//     <div className="container mx-auto p-3 h-screen overflow-auto">
+//       {/* heading */}
+//       <div className="flex justify-between items-center">
+//         <h3 className="text-2xl font-bold">
+//           Reading list (
+//           {readList?.data.length > 0 ? readList?.data.length : "0"})
+//         </h3>
+//         <input
+//           type="text"
+//           placeholder="Search here..."
+//           // value={searchTerm}
+//           // onChange={handleSearch}
+//           className="px-2 py-1 border border-gray-300 rounded-md"
+//         />
+//       </div>
+
+//       {/* content */}
+
+//       {readList?.data.length > 0
+//         ? readList?.data.map((item, index) => (
+//             <div key={index} className="p-5 flex justify-between items-center">
+//               <a href={`/post/read?slug=${item.slug}`}>
+//                 <div className="flex justify-between items-center">
+//                   <div className="flex items-center gap-3">
+//                     <img
+//                       className="w-12 h-12 object-cover pb-2 pr-2 rounded-md"
+//                       src={item.picture}
+//                       alt={item.title}
+//                     />
+//                     <div>
+//                       <h6>{item.title}</h6>
+//                       <div className="flex">
+//                         <p className="mr-4">{item.userName}</p>
+//                         <p className="mr-4">
+//                           {new Date(item.updatedAt).toDateString()}
+//                         </p>
+//                         <p>{item.readTime} minutes</p>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </a>
+//               <Button
+//                 size="xs"
+//                 color="error"
+//                 onClick={() => handleShow(item.postId)}
+//               >
+//                 Remove
+//               </Button>
+//             </div>
+//           ))
+//         : "NO Read List Found"}
+
+//       {isLoading && <Spinner />}
+
+//       <div>
+//         <ConfirmationModal
+//           show={show}
+//           handleShow={handleShow}
+//           action={handleUpdateThunk}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ReadList;
+
+
+
+import { BookmarkSimple, CloudArrowUp } from "phosphor-react";
 import { useDispatch, useSelector } from "react-redux";
-import Spinner from "../../components/Spinner/Spinner";
-
-import ConfirmationModal from "../../components/Modal/ConfirmationModal";
-import { useState } from "react";
 import { updateReadListThunk } from "../../redux/readList/readListSlice";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { successNotification } from "../../utilities/NotificationHelper";
 
-const ReadList = () => {
+const ReadList = ({ postId }) => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [show, setShow] = useState(false);
-  const [postId, setPostId] = useState("");
-  const { readList, isLoading } = useSelector((state) => state.readList);
+  const { readList } = useSelector((state) => state.readList);
+  const { user } = useSelector((state) => state.auth);
+  const isBooked = readList?.data.find((item) => item.postId === postId);
 
-  const handleUpdateThunk = () => {
-    dispatch(updateReadListThunk(postId));
+  const [showModal, setShowModal] = useState(false);
+
+  const handleReadList = () => {
+    if (user?.data && user?.token) {
+      dispatch(updateReadListThunk(postId))
+        .unwrap()
+        .then(() => {
+          successNotification("আপনার কাজ সফল হয়েছে ");
+        });
+    } else {
+      setShowModal(true);
+    }
   };
 
-  const handleShow = (postId) => {
-    setPostId(postId);
-    setShow(!show);
+  const onClickModalCancel = () => {
+    setShowModal(false);
+  };
+
+  const onClickModalLogin = () => {
+    setShowModal(false);
+    navigate("/login");
   };
 
   return (
-    <div className="container mx-auto p-3 h-screen overflow-auto">
-      {/* heading */}
-      <div className="flex justify-between items-center">
-        <h3 className="text-2xl font-bold">
-          Reading list (
-          {readList?.data.length > 0 ? readList?.data.length : "0"})
-        </h3>
-        <input
-          type="text"
-          placeholder="Search here..."
-          // value={searchTerm}
-          // onChange={handleSearch}
-          className="px-2 py-1 border border-gray-300 rounded-md"
-        />
+    <>
+      <div className="cursor-pointer" onClick={handleReadList}>
+        <BookmarkSimple size={24} style={{ color: `${isBooked ? "red" : ""}` }} />
       </div>
 
-      {/* content */}
-
-      {readList?.data.length > 0
-        ? readList?.data.map((item, index) => (
-            <div key={index} className="p-5 flex justify-between items-center">
-              <a href={`/post/read?slug=${item.slug}`}>
-                <div className="flex justify-between items-center">
-                  <div className="flex items-center gap-3">
-                    <img
-                      className="w-12 h-12 object-cover pb-2 pr-2 rounded-md"
-                      src={item.picture}
-                      alt={item.title}
-                    />
-                    <div>
-                      <h6>{item.title}</h6>
-                      <div className="flex">
-                        <p className="mr-4">{item.userName}</p>
-                        <p className="mr-4">
-                          {new Date(item.updatedAt).toDateString()}
-                        </p>
-                        <p>{item.readTime} minutes</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
-              <Button
-                size="xs"
-                color="error"
-                onClick={() => handleShow(item.postId)}
-              >
-                Remove
-              </Button>
-            </div>
-          ))
-        : "NO Read List Found"}
-
-      {isLoading && <Spinner />}
-
-      <div>
-        <ConfirmationModal
-          show={show}
-          handleShow={handleShow}
-          action={handleUpdateThunk}
-        />
-      </div>
-    </div>
+      {/* Modal Code */}
+      {showModal && (
+        <div className="fixed inset-0 overflow-y-auto z-50 flex items-center justify-center">
+          <div className="flex fixed inset-0 bg-black bg-opacity-50"></div>
+          <div className="bg-white w-96 p-4 rounded-md z-10">
+            <CloudArrowUp size={28} color="#1B4DFF" />
+            <p className="text-body-5 md:text-body-4 leading-relaxed text-metal-500">
+              You are not logged in. Please login first.
+            </p>
+            <button
+              className="mt-4 px-4 py-2 bg-[#1B4DFF] text-white rounded-md"
+              onClick={onClickModalCancel}
+            >
+              Close
+            </button>
+            <button
+              className="mt-4 px-4 py-2 bg-[#1B4DFF] text-white rounded-md ml-2"
+              onClick={onClickModalLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 export default ReadList;
+
